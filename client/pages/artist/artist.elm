730d5c027093462ea048d4713bd299a23b00fdd8
simonewebdesign <hello@simonewebdesign.it>
2016-01-08 14:46:23 +0000

    Refactoring; decouple Artist from Model and prepare for implementing CRUD actions

--- client/pages/artist/artist.elm
+++ client/pages/artist/artist.elm
@@ -2,13 +2,19 @@ module Artist where
 
 import Debug
 import Html exposing (..)
-import Html.Attributes exposing (class)
+import Html.Attributes as Attr
+import Html.Events exposing (on, onClick, targetValue)
 import Http
 import Json.Decode exposing ((:=))
 import Task exposing (Task, andThen)
 
 
 -- MODEL
+
+type alias Model =
+  { inputText : String
+  , artists : List Artist
+  }
 
 type alias Artist =
   { id: Int
@@ -26,13 +32,12 @@ decoder =
   in
     "data" := Json.Decode.list artist
 
-type alias Model =
-  List Artist
 
-
-init : Model
-init =
-  []
+initialModel : Model
+initialModel =
+  { inputText = ""
+  , artists = []
+  }
 
 
 -- UPDATE
@@ -40,6 +45,10 @@ init =
 type Action
   = NoOp
   | SetArtists (List Artist)
+  --| Create
+  --| Update Int
+  --| Delete Int
+  --| UpdateInputText String
 
 
 update : Action -> Model -> Model
@@ -48,19 +57,35 @@ update action model =
     NoOp ->
       model
 
-    SetArtists model' ->
-      model'
+    SetArtists artists ->
+      { model | artists = artists }
+
+    --Create ->
+    --  model |> createArtist model.inputText
+
+    --Update id ->
+    --  model
+
+    --Delete id ->
+    --  model
+
+    --UpdateInputText txt ->
+    --  model
+
+--createArtist : String -> Model -> Model
+--createArtist name model =
+--  { model | artists = {id = 0, name = name} :: model.artists }
 
 
 -- SIGNALS
 
 main : Signal Html
 main =
-  Signal.map view model
+  Signal.map view state
 
 
-model : Signal Model
-model = Signal.foldp update init actions.signal
+state : Signal Model
+state = Signal.foldp update initialModel actions.signal
 
 
 actions : Signal.Mailbox Action
@@ -85,11 +110,39 @@ view model =
   let th' field = th [] [text field]
       tr' artist = tr [] [ td [] [text <| toString artist.id]
                          , td [] [text <| artist.name]
+                         , td []
+                           [ button [Attr.type' "button", Attr.class "btn btn-danger"] [text "Delete"]
+                           ]
                          ]
   in
-    div [class "container"]
-    [ table [class "table table-striped table-bordered"]
-      [ thead [] [tr [] (List.map th' ["ID", "Name"])]
-      , tbody [] (List.map tr' model)
+    div [Attr.class "container"]
+
+    [ entryForm model
+    , table [Attr.class "table table-striped table-bordered"]
+      [ thead [] [tr [] (List.map th' ["ID", "Name", "Actions"])]
+      , tbody [] (List.map tr' model.artists)
       ]
     ]
+
+
+entryForm : Model -> Html
+entryForm model =
+  div [ ]
+  [ input
+    [ Attr.type' "text"
+    , Attr.placeholder "Artist name..."
+    , Attr.value model.inputText
+    , Attr.name "artist"
+    , Attr.autofocus True
+    --, onInput address UpdateInputText
+    ][]
+  --, button [ Attr.class "add", onClick actions.address Create ] [ text "Add" ]
+  , h2 [] [
+      text (model.inputText ++ " << this is the text entered ")
+    ]
+  ]
+
+
+onInput : Signal.Address a -> (String -> a) -> Attribute
+onInput address contentToValue =
+    on "input" targetValue (\str -> Signal.message address (contentToValue str))
